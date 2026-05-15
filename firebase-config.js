fcb13952aa2a0fc88668be213062272fb6c04f38
hansoku1-crypto <hansoku1@az-zone.com>
2026-05-15 14:32:42 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,17 @@
-// Replace these values with the Firebase web app config from your Firebase Console.
-// Leave apiKey empty to keep using localStorage while developing the prototype.
-window.CLUBLOG_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWrRYYCx63F8rAXh7qoes3Uk_81cZ8dbA",
+  authDomain: "azism-bukatsu.firebaseapp.com",
+  projectId: "azism-bukatsu",
+  storageBucket: "azism-bukatsu.firebasestorage.app",
+  messagingSenderId: "369499792118",
+  appId: "1:369499792118:web:2694cdfb87981fdb8142d7"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
